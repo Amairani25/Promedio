4c413154e98a791fe1f86b61c27989f0cae34849
amair <amair@LAPTOP-Q9P3M1OQ>
2017-11-10 23:00:58 -0600

    Add project files.

---
+++ Promedio/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Promedio
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int calificacion = 1;
+                int acumulador = 0;
+                int n = 0;
+                while (calificacion > 0)
+                {
+                    Console.WriteLine("Introduce tu calificacion: ");
+                    calificacion = int.Parse(Console.ReadLine());
+                    acumulador = acumulador + calificacion;
+                    n = n + 1;
+                }
+                int promedio;
+                Console.WriteLine("La suma de tu calificacion es: " + acumulador);
+                promedio = acumulador / n;
+
+                Console.WriteLine("Tu promedio es: " + promedio);
+                Console.ReadKey();
+            }
+
+        }
+    }
+
+
+}
